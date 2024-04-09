f3e14a4931167e8bb7494667e52bae6870081f10
tnmcat <110289137+tnmcat@users.noreply.github.com>
2024-04-09 19:01:59 +0700

    add headless/ui and filter data (uncomplete)

--- cg-forestdise-app-dev/src/pages/Home.js
+++ cg-forestdise-app-dev/src/pages/Home.js
@@ -1,17 +1,18 @@
 import React from "react";
 import Banner from "../components/main/home/Banner";
 import Products from "../components/main/home/Products";
+import Product from "../components/main/product/product";
 
 function Home() {
-
-  return (
-    <div className="font-bodyFont bg-gray-100">
-      <Banner />
-      <div className="relative w-full -mt-10 xl:-mt-36 py-10 z-0">
-        <Products />
-      </div>
-    </div>
-  );
+    return (
+        <div className="font-bodyFont bg-gray-100">
+            <Banner />
+            <div className="relative w-full -mt-10 xl:-mt-36 py-10 z-0">
+                {/* <Products /> */}
+                <Product />
+            </div>
+        </div>
+    );
 }
 
 export default Home;
