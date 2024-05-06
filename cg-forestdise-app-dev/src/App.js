9802bdaa93534736922148ededc8329ec0fa6db2
tnmcat <110289137+tnmcat@users.noreply.github.com>
2024-05-06 23:38:40 +0700

    nothing update

--- cg-forestdise-app-dev/src/App.js
+++ cg-forestdise-app-dev/src/App.js
@@ -10,47 +10,25 @@ import {
 import Home from "./pages/Home";
 import Cart from "./components/main/cart/Cart";
 import Signin from "./pages/Signin";
-import HomeContent from "./components/main/store/HomeContent";
-import DealsContent from "./components/main/store/DealsContent";
-import CategoryContent from "./components/main/store/CategoryContent";
-import SearchContent from "./components/main/store/SearchContent";
 import Error from "./pages/Error";
 import Registration from "./pages/Registration";
 import Header from "./components/common/header/Header";
 import Footer from "./components/common/footer/Footer";
-import ProductDetail from "./components/main/variant/ProductDetail";
-import SellingHeader from "./components/common/sellingheader/SellingHeader";
-import Selling from "./components/main/selling/Selling";
-import ShopCreat from "./components/main/selling/main/ShopCreate";
-import Category from "./components/main/selling/main/Category";
 import Payment from "./pages/Payment";
 import StoreHeader from "./components/main/store/StoreHeader";
 import StoreFooter from "./components/main/store/StoreFooter";
 import StoreBanner from "./components/main/store/StoreBanner";
-import Variants from "./components/main/selling/main/Variants";
-import Product from "./components/main/selling/main/Product";
-import Offers from "./components/main/selling/main/Offers";
-import Dashboard from "./components/main/dashboard/Dashboard";
 import DashBoardHeader from "./components/main/dashboard/DashBoardHeader";
-import Categories from "./components/main/dashboard/element/Categories";
-import HomeDasboard from "./components/main/dashboard/element/HomeDasboard";
-import OrdersDashboard from "./components/main/dashboard/element/OrdersDashboard";
-import Products from "./components/main/dashboard/element/Products";
-import Profile from "./components/main/dashboard/element/Profile";
-import Images from "./components/main/selling/main/Images";
-import HomeSelling from "./components/main/selling/main/HomeSelling";
-import SellerRegistration from "./pages/SellerRegistration";
-import SellerSignin from "./pages/SellerSignin";
-import SubCategoryContent from "./components/main/store/SubCategoryContent";
 import Confirm from "./pages/Confirm";
 import ShowProduct from "./components/main/product/product";
 import ForgotPassword from "./pages/ForgotPassword";
 import UserProfile from "./components/main/profile/UserProfile";
 import Success from "./pages/Success";
-import UserOrder from "./components/main/order/UserOrder";
-import Review from "./components/main/variant/Review";
 import OrderReview from "./components/main/order/OrderReview";
 import Order from "./components/main/order/Order";
+import {Dashboard} from "@mui/icons-material";
+import ProductDetail from "./components/main/variant/ProductDetail";
+
 const Layout = () => {
     return (
         <div>
@@ -61,17 +39,7 @@ const Layout = () => {
         </div>
     );
 };
-const SellingLayout = () => {
-    return (
-        <div>
-            <SellingHeader />
-            <Selling>
-                <Outlet />
-            </Selling>
-            <Footer />
-        </div>
-    );
-};
+
 const DashboardLayout = () => {
     return (
         <div>
@@ -93,7 +61,6 @@ const StoreLayout = () => {
         </div>
     );
 };
-
 function App() {
     const router = createBrowserRouter(
         createRoutesFromElements(
@@ -103,14 +70,6 @@ function App() {
                 <Route path="/register" element={<Registration />} />
                 <Route path="/signin" element={<Signin />} />
                 <Route path="/changepass" element={<ForgotPassword />} />
-                <Route
-                    path="/sellercentral/register"
-                    element={<SellerRegistration />}
-                />
-                <Route
-                    path="/sellercentral/signin"
-                    element={<SellerSignin />}
-                />
 
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Home />} />
@@ -122,50 +81,9 @@ function App() {
                     ></Route>
                     <Route path="/product/:id" element={<ProductDetail />} />
                     <Route path="/review/:id" element={<OrderReview />} />
-                    <Route path="/store/:id" element={<StoreLayout />}>
-                        <Route index element={<HomeContent />} />
-                        <Route
-                            path="/store/:id/deals"
-                            element={<DealsContent />}
-                        ></Route>
-                        <Route
-                            path="/store/:id/search"
-                            element={<SearchContent />}
-                        ></Route>
-                        <Route
-                            path="/store/:id/:categoryName"
-                            element={<CategoryContent />}
-                        ></Route>
-                        <Route
-                            path="/store/:id/:categoryName/:subCategoryName"
-                            element={<SubCategoryContent />}
-                        ></Route>
-                    </Route>
-                </Route>
-                <Route path="/payment" element={<Payment />} />
-                <Route path="/success" element={<Success />} />
-                <Route path="/order" element={<Order />} />
-                <Route path="/selling" element={<SellingLayout />}>
-                    <Route index element={<HomeSelling />} />
-                    <Route path="/selling/shop" element={<ShopCreat />} />
-                    <Route path="/selling/category" element={<Category />} />
-                    <Route path="/selling/product" element={<Product />} />
-                    <Route path="/selling/attributes" element={<Offers />} />
-                    <Route path="/selling/variant" element={<Variants />} />
-                    <Route path="/selling/images" element={<Images />} />
-                </Route>
-                <Route path="/dashboard" element={<DashboardLayout />}>
-                    <Route index element={<HomeDasboard />} />
-                    <Route
-                        path="/dashboard/categories"
-                        element={<Categories />}
-                    />
-                    <Route path="/dashboard/products" element={<Products />} />
-                    <Route
-                        path="/dashboard/orders"
-                        element={<OrdersDashboard />}
-                    />
-                    <Route path="/dashboard/profile" element={<Profile />} />
+                    <Route path="/payment" element={<Payment />} />
+                    <Route path="/success" element={<Success />} />
+                    <Route path="/order" element={<Order />} />
                 </Route>
             </Fragment>
         )
