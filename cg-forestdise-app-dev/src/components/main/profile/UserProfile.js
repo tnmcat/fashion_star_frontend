c299bba6b83b71640df34a3b67af4a7fee714180
chienpham1605 <107631195+chienpham1605@users.noreply.github.com>
2024-05-15 09:47:03 +0700

    git update.

--- cg-forestdise-app-dev/src/components/main/profile/UserProfile.js
+++ cg-forestdise-app-dev/src/components/main/profile/UserProfile.js
@@ -1,53 +1,50 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { Box, Button, Grid, TextField, FormControlLabel, Checkbox, Card, CardContent, Typography, Link, Avatar, Modal } from '@mui/material';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import LockIcon from '@mui/icons-material/Lock';
-import PhoneIcon from '@mui/icons-material/Phone';
-import EmailIcon from '@mui/icons-material/Email';
-import { getUserInfo } from '../../../features/user/userSlice';
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import {
+    Box,
+    Button,
+    Grid,
+    TextField,
+    FormControlLabel,
+    Checkbox,
+    Card,
+    CardContent,
+    Typography,
+    Link,
+    Avatar,
+    Modal,
+} from "@mui/material";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import LockIcon from "@mui/icons-material/Lock";
+import PhoneIcon from "@mui/icons-material/Phone";
+import EmailIcon from "@mui/icons-material/Email";
+import { getUserInfo } from "../../../features/user/userSlice";
 
 const UserProfileEditForm = () => {
     const navigate = useNavigate();
     const dispatch = useDispatch();
-    const userId = useSelector(state => state.user.userId); // Lấy userId từ Redux store
+    const userId = useSelector((state) => state.user.userId);
+    const userInfo = useSelector((state) => state.user);
 
     const [formData, setFormData] = useState({
-        clientName: '',
-        birthday: '',
-        phoneNumber: '',
-        email: '',
-        gender: '',
+        clientName: "",
+        birthday: "",
+        phoneNumber: "",
+        gender: "",
     });
 
     const [modalOpen, setModalOpen] = useState(false);
 
     useEffect(() => {
-
-        const fetchUserInfo = async () => {
-            try {
-                const response = await fetch(`http://localhost:5454/api/users/${userId}`);
-                if (response.ok) {
-                    const data = await response.json();
-                    // Lưu thông tin người dùng vào state formData
-                    setFormData({
-                        clientName: data.clientName,
-                        birthday: data.birthday,
-                        phoneNumber: data.phone,
-                        email: data.email,
-                        gender: data.gender,
-                    });
-                } else {
-                    // Xử lý khi không thể lấy thông tin người dùng
-                }
-            } catch (error) {
-                console.error('Error:', error);
-            }
-        };
-
-        fetchUserInfo();
-    }, [userId]);
+        // Lấy thông tin người dùng từ Redux store và cập nhật vào state formData
+        setFormData({
+            clientName: userInfo.clientName,
+            birthday: userInfo.birthday,
+            phoneNumber: userInfo.phone,
+            gender: userInfo.gender,
+        });
+    }, [userInfo]);
 
     const handleChange = (event) => {
         const { name, value } = event.target;
@@ -71,27 +68,35 @@ const UserProfileEditForm = () => {
             }
 
             if (!isPhoneValid) {
-                alert("Phone number must be 10 digits and contain only numbers.");
+                alert(
+                    "Phone number must be 10 digits and contain only numbers."
+                );
                 return;
             }
 
-            const response = await fetch(`http://localhost:5454/api/users/${userId}/edit`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    // Add authorization token if needed
-                },
-                body: JSON.stringify(formData),
-            });
+            // Gửi yêu cầu chỉnh sửa thông tin người dùng đến API
+            const response = await fetch(
+                `http://localhost:5454/api/users/${userId}/edit`,
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        // Add authorization token if needed
+                    },
+                    body: JSON.stringify(formData),
+                }
+            );
+
             if (response.ok) {
-                const data = await response.json();
-                // Mở modal thông báo cập nhật thành công
+                // Nếu yêu cầu thành công, hiển thị thông báo thành công
                 setModalOpen(true);
             } else {
-                // Xử lý lỗi khi gửi yêu cầu
+                // Nếu có lỗi từ API, xử lý lỗi
+                const errorData = await response.json();
+                alert(errorData.message); // Hiển thị thông báo lỗi từ API
             }
         } catch (error) {
-            console.error('Error:', error);
+            console.error("Error:", error);
         }
     };
 
@@ -143,36 +148,32 @@ const UserProfileEditForm = () => {
                                     value={formData.phoneNumber}
                                     onChange={handleChange}
                                     InputProps={{
-                                        startAdornment: (
-                                            <PhoneIcon />
-                                        ),
+                                        startAdornment: <PhoneIcon />,
                                     }}
                                 />
                             </Grid>
-                            <Grid item xs={12} sm={6}>
-                                <TextField
-                                    required
-                                    id="email"
-                                    name="email"
-                                    label="Email"
-                                    fullWidth
-                                    autoComplete="email"
-                                    value={formData.email}
-                                    onChange={handleChange}
-                                    InputProps={{
-                                        startAdornment: (
-                                            <EmailIcon />
-                                        ),
-                                    }}
-                                />
-                            </Grid>
+
                             <Grid item xs={12} sm={6}>
                                 <FormControlLabel
-                                    control={<Checkbox color="primary" value="male" name="gender" onChange={handleChange} />}
+                                    control={
+                                        <Checkbox
+                                            color="primary"
+                                            value="male"
+                                            name="gender"
+                                            onChange={handleChange}
+                                        />
+                                    }
                                     label="Male"
                                 />
                                 <FormControlLabel
-                                    control={<Checkbox color="primary" value="female" name="gender" onChange={handleChange} />}
+                                    control={
+                                        <Checkbox
+                                            color="primary"
+                                            value="female"
+                                            name="gender"
+                                            onChange={handleChange}
+                                        />
+                                    }
                                     label="Female"
                                 />
                             </Grid>
@@ -195,20 +196,33 @@ const UserProfileEditForm = () => {
                 </Grid>
                 <Grid item xs={6} lg={4}>
                     <Card>
-                        <CardContent sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                        <CardContent
+                            sx={{
+                                display: "flex",
+                                flexDirection: "column",
+                                alignItems: "center",
+                            }}
+                        >
                             <Avatar sx={{ width: 100, height: 100, mb: 2 }}>
                                 <AccountCircleIcon />
                             </Avatar>
                             <Typography variant="h6" gutterBottom>
-                                {formData.clientName} {/* Hiển thị tên người dùng */}
+                                {formData.clientName}
+                                {/* Hiển thị tên người dùng từ Redux store */}
                             </Typography>
                             <Typography gutterBottom>
-                                <PhoneIcon /> {formData.phoneNumber} {/* Hiển thị số điện thoại */}
+                                <PhoneIcon /> {formData.phoneNumber}
+                                {/* Hiển thị số điện thoại từ Redux store */}
                             </Typography>
+                            {/* Hiển thị email từ Redux store */}
                             <Typography gutterBottom>
-                                <EmailIcon /> {formData.email} {/* Hiển thị email */}
+                                <EmailIcon /> {userInfo.email}
                             </Typography>
-                            <Link href="/change-password" color="primary" underline="hover">
+                            <Link
+                                href="/change-password"
+                                color="primary"
+                                underline="hover"
+                            >
                                 <LockIcon /> Change Password
                             </Link>
                         </CardContent>
@@ -223,18 +237,24 @@ const UserProfileEditForm = () => {
                 aria-labelledby="modal-modal-title"
                 aria-describedby="modal-modal-description"
             >
-                <Box sx={{
-                    position: 'absolute',
-                    top: '50%',
-                    left: '50%',
-                    transform: 'translate(-50%, -50%)',
-                    width: 400,
-                    bgcolor: 'background.paper',
-                    border: '2px solid #000',
-                    boxShadow: 24,
-                    p: 4,
-                }}>
-                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                <Box
+                    sx={{
+                        position: "absolute",
+                        top: "50%",
+                        left: "50%",
+                        transform: "translate(-50%, -50%)",
+                        width: 400,
+                        bgcolor: "background.paper",
+                        border: "2px solid #000",
+                        boxShadow: 24,
+                        p: 4,
+                    }}
+                >
+                    <Typography
+                        id="modal-modal-title"
+                        variant="h6"
+                        component="h2"
+                    >
                         Update Successful
                     </Typography>
                     <Typography id="modal-modal-description" sx={{ mt: 2 }}>
